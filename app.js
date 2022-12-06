b645303fb799c0c70348398afdfd97b5ffc1f0e7
Fred Gohsman <fredgohsman@microsoft.com>
2022-12-06 15:11:09 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// This is a sample JavaScript file
+const method = () => {
+  const v1 = 'abcdefg'
+  let v2 = 24
+  
+  if (v2 == '24')
+    console.log('v2 is more or less 24')    
+}
+
+method()
